555224d763f3cb62b4c3b936cd9d51da83558c89
Wherewasjeff <144689068+Wherewasjeff@users.noreply.github.com>
2023-09-11 15:10:42 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("click").addEventListener("click",()=> {
+ const inputValue = document.createElement("h1")
+ const h1=document= document.createElement("h1");
+    
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    
+    var userInput = "Ievadītais vārds: " + firstName + " " + lastName;
+  });
+  
