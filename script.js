eb4380be82dddeb4a151793db22a2eab60ad7124
Wherewasjeff <144689068+Wherewasjeff@users.noreply.github.com>
2023-09-13 14:35:59 +0300

    Versija2

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-document.getElementById("click").addEventListener("click",()=> {
- const inputValue = document.createElement("h1")
- const h1=document= document.createElement("h1");
-    
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    
-    var userInput = "Ievadītais vārds: " + firstName + " " + lastName;
-  });
+document.getElementById("click").addEventListener("click", () => {
+  var firstName = document.getElementById("firstName").value;
+  var lastName = document.getElementById("lastName").value;
+
+  var userInput = "Ievadītais vārds: " + firstName + " " + lastName;
   
+  // Create a new <h1> element to display the user input
+  var inputValue = document.createElement("h1");
+  inputValue.textContent = userInput;
+
+  // Get the div where you want to display the output
+  var outputDiv = document.getElementById("output");
+
+  // Append the <h1> element to the output div
+  outputDiv.appendChild(inputValue);
+});
